1ca46b5ef2bb309ddc95429cfb0f2fc9dd3db679
Jasper Reed <jmixon.reed@gmail.com>
2020-07-14 18:59:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -116,6 +116,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    
+    programmer_hash
 
 end
 
@@ -138,5 +142,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+    
+    programmer_hash
 
 end
