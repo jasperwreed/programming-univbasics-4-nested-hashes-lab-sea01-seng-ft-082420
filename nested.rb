c42d4ac33d0af74ea7acd38ad227baabdf123513
Jasper Reed <jmixon.reed@gmail.com>
2020-07-14 18:53:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
+    
+    puts programmer_hash[:grace_hopper]
 
 end
 
@@ -36,6 +38,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    
+    puts programmer_hash[alan_kay][known_for]
 
 end
 
@@ -55,6 +59,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+    puts programmer_hash[dennis_ritchie][languages][0]
 
 end
 
